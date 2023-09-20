1ce1c568b93e95641425e37a7c6afc7a4d41c21a
franjunoy <franjunoy@hotmail.com>
2023-09-20 16:18:03 -0300

    Cambio funcion main

--- Algoritmo de Euclides/main.c
+++ Algoritmo de Euclides/main.c
@@ -10,4 +10,16 @@ int mcd(fraccion *f); // calcula el m�nimo com�n divisor y lo retorna
 fraccion reduce (fraccion *d); // retorna la fracci�n reducida
 void muestraFraccion (fraccion c); // imprime por pantalla los valores
 
+int main(){
+    fraccion f1 = {140, 26};
+    fraccion f2 = {380, 44};
+    fraccion f3 = {830, 675};
+    muestraFraccion (reduce(&f1));
+    muestraFraccion (reduce(&f2));
+    muestraFraccion (reduce(&f3));
+
+return 0;
+}
+
+
 
